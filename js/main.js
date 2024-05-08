fdd232e0d73ff28b239d4fa738ddbaaf3423cf01
EvgMarty <mrtevgeniy@gmail.com>
2024-05-08 20:23:59 +0300

    tabs footer and swiper

--- js/main.js
+++ js/main.js
@@ -23,3 +23,33 @@ function closeHint() {
     });
   });
 }
+
+// swiper
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 4,
+  spaceBetween: 42,
+  loop: true,
+  freeMode: true,
+
+  // breakpoints: {
+  //   640: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 20,
+  //   },
+  //   768: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 40,
+  //   },
+  //   1024: {
+  //     slidesPerView: 4,
+  //     spaceBetween: 50,
+  //   },
+  // },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '#slider-next',
+    prevEl: '#slider-prev',
+  },
+});
